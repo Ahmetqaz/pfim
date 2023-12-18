38d1e75c9afb496d8a531d0b0ba428c819b52bbd
Ahmet <ahmetcanlyev@gmail.com>
2023-12-18 19:11:59 +0500

    desktop version

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ if (header)
       header.classList.remove("sticky");
     }
   });
-  
+
 // const links = document.querySelectorAll(".links");
 // const sections = document.querySelectorAll(".anchor");
 // function changeLinkState() {
@@ -65,37 +65,36 @@ if (header)
 // new WOW().init({
 //   boxClass: "wow",
 // });
-// const tabBtn = document.querySelectorAll(".tabBtn");
-// const tabEvent = document.querySelectorAll(".tabEvent");
-// tabBtn.forEach((e) => {
-//   onTabClick(tabBtn, tabEvent, e);
-// });
-// function onTabClick(tabBtns, tabItems, item) {
-//   item.addEventListener("click", function (e) {
-//     let currentBtn = item;
-//     let tabId = currentBtn.getAttribute("data-tab");
-//     let currentTab = document.querySelector(tabId);
-//     if (currentBtn.classList.contains("active")) {
-//       console.log("now active");
-//       const faq = currentBtn.parentElement.querySelector(".tabEvent");
-//       if (faq) {
-//         faq.classList.remove("active");
-//         currentBtn.classList.remove("active");
-//       }
-//     } else if (!currentBtn.classList.contains("active")) {
-//       tabBtns.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      console.log("now active");
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
 
-//       tabItems.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-//       currentBtn.classList.add("active");
-//       currentTab.classList.add("active");
-//     }
-//   });
-// }
-
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
 
 // video ----------------
 // const video = document.querySelector("#video");
@@ -172,58 +171,3 @@ if (header)
 //   };
 // }
 
-// counter ------------
-// const amounts = document.querySelector(".amounts");
-// if (amounts) {
-//   const animationDuration = 2000;
-//   const frameDuration = 1000 / 60;
-//   const totalFrames = Math.round(animationDuration / frameDuration);
-//   const easeOutQuad = (t) => t * (2 - t);
-//   let isCounting = true;
-//   const animateCountUp = (el) => {
-//     let frame = 0;
-//     const countTo = parseInt(el.getAttribute("data-count"), 10);
-//     const counter = setInterval(() => {
-//       frame++;
-//       const progress = easeOutQuad(frame / totalFrames);
-//       const currentCount = Math.round(countTo * progress);
-//       if (parseInt(20, 10) !== currentCount) {
-//         el.innerHTML = currentCount;
-//       }
-//       if (frame === totalFrames) {
-//         clearInterval(counter);
-//       }
-//     }, frameDuration);
-//   };
-//   const runAnimations = () => {
-//     const countupEls = document.querySelectorAll(".counter");
-//     countupEls.forEach(animateCountUp);
-//   };
-//   const isVisible = function (elem) {
-//     var bounding = elem.getBoundingClientRect();
-//     return (
-//       bounding.top >= 0 &&
-//       bounding.left >= 0 &&
-//       bounding.bottom <=
-//         (window.innerHeight || document.documentElement.clientHeight) &&
-//       bounding.right <=
-//         (window.innerWidth || document.documentElement.clientWidth)
-//     );
-//   };
-//   document.addEventListener("scroll", () => {
-//     const visible = isVisible(amounts);
-//     if (visible && isCounting) {
-//       runAnimations();
-//       isCounting = false;
-//     }
-//   });
-// }
-// counter ------------
-
-// let scrollTop = document.getElementById("scrollTop");
-// if (scrollTop) {
-//   scrollTop.onclick = () => {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-//   };
-// }
